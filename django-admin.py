5f8d1a8fcde42b8f5f80dbcb077ae158a168a7fa
Dima <dima@gmail.com>
2016-07-12 16:13:58 +0300

    First Django

---
+++ django-admin.py
@@ -0,0 +1,4 @@
+#!/home/dima/django/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.6.1','django-admin.py'
+__requires__ = 'Django==1.6.1'
+__import__('pkg_resources').run_script('Django==1.6.1', 'django-admin.py')
